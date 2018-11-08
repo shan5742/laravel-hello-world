d91b908dbd90f31fde9c2e38742a06f7ca59467e
shan5742 <asamshan456@gmail.com>
2018-11-08 21:07:47 +0000

    basic blade templating and creating layouts

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name')}}</title>
-    </head>
-    <body>
-        <h1>About</h1>
-        <p>This is my about page</p>    
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>About</h1>
+    <p>This is my about page</p>    
+@endsection
